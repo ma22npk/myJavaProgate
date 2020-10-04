7ce04f654ce27f4e751dd312300456c8e802f63a
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-10-04 09:26:20 +0900

    出力を命令する

---
+++ MyApp.java
@@ -0,0 +1,7 @@
+// Javaを実行するにはclassとその中にメインメソッドが必要。
+class MyApp {
+  public static void main(String[] args) {
+    // System.out.println()はjavascriptのconsole.logのようなもの
+    System.out.println("javaの学習を開始");
+  }
+}
