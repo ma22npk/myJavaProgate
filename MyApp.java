5d51343074a5686ffb3594c54256854412e688d2
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-10-04 09:41:30 +0900

    数値を扱い計算する

--- MyApp.java
+++ MyApp.java
@@ -1,7 +1,23 @@
 // Javaを実行するにはclassとその中にメインメソッドが必要。
+//クラス部分
 class MyApp {
+  // メソッド部分
   public static void main(String[] args) {
+    // 処理部分
     // System.out.println()はjavascriptのconsole.logのようなもの
-    System.out.println("javaの学習を開始");
+    // Javaでは文の終わりにセミコロンをつけるのを忘れないように。
+    // System.out.println("javaの学習を開始");
+    // 数値を扱う場合はクオーテーションで囲まない。
+    System.out.println(2);
+    //計算も可能
+    System.out.println(2 + 3);
+    //掛け算
+    System.out.println(10 * 7);
+    //割り算
+    System.out.println(9 / 3);
+    //余りを表示
+    System.out.println(10 % 7);
+    // 文字列として認識
+    System.out.println("10 - 7");
   }
 }
